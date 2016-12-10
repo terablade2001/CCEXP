b216d34e9cbbda1b885d6087f4ccb4ecd76c6a3b
terablade2001 <terablade2001@yahoo.gr>
2016-12-10 21:34:06 +0200

    [0.060]: [+]: Updating Copyright notices including URL to library.

--- Python/ccexp/setup.py
+++ Python/ccexp/setup.py
@@ -1,6 +1,7 @@
 # MIT License
 
 # Copyright (c) 2016 Vasileios Kon. Pothos (terablade2001)
+# https://github.com/terablade2001/CCEXP
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
